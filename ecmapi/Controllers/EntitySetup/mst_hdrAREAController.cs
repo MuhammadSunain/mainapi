fa3c7484e1a8c0691e4af0f1ba865c73aad8ad16
Zeeshan.hanif <zeeshan.hanif@digicopsolutions@com>
2023-05-16 17:44:18 +0500

    Site and area screen api Done

---
+++ ecmapi/Controllers/EntitySetup/mst_hdrAREAController.cs
@@ -0,0 +1,35 @@
+﻿using ecmapi.dto;
+using ecmapi.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace ecmapi.Controllers.EntitySetup
+{
+    [RoutePrefix("api/v1/hdr_MST_area")]
+    public class AreaController : BaseController<mst_hdrArea>
+    {
+
+        ecomSchoolEntities db = new ecomSchoolEntities();
+
+        [HttpPost]
+        [Route("Save/{area}")]
+        public IHttpActionResult SaveHdrAREA(mst_hdrArea area)
+        {
+            try
+            {
+                Save(area);
+            }
+            catch(Exception)
+            {
+                throw;
+            }
+            return Ok(area);
+        }
+    }
+}
---
+++ ecmapi/Controllers/EntitySetup/mst_hdr_siteController.cs
@@ -0,0 +1,54 @@
+﻿using ecmapi.dto;
+using ecmapi.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace ecmapi.Controllers.EntitySetup
+{
+    [RoutePrefix("api/v1/mst_hdr_site")]
+    public class hdr_SiteController : BaseController<mst_hdr_site>
+    {
+
+        ecomSchoolEntities db = new ecomSchoolEntities();
+
+        [HttpPost]
+        [Route("Save/{obj}")]
+        public IHttpActionResult SaveMst_HDR_site(mst_hdr_site obj)
+        {
+            try
+            {
+                Save(obj);
+            }
+            catch(Exception)
+            {
+                throw;
+            }
+            return Ok(obj);
+        }
+        [HttpGet]
+        [Route("getMst_HDR_Sites_entityWise/{id}")]
+        public async Task<List<mstHdrSite>> getMst_HDR_Sites_entityWise(int id)
+        {
+            var query = (from hdr in db.mst_hdr_site
+                         where
+                         (id == hdr.entityId)
+                         select new mstHdrSite()
+                         {
+                             Id = hdr.Id,
+                             code = hdr.code,
+                             sitename = hdr.sitename,
+                             sitetype = hdr.sitetype,
+                             sitefor = hdr.sitefor,
+                             region = hdr.region,
+                             entityId = hdr.entityId
+                         }).ToList();
+            return query;
+        }
+    }
+}
